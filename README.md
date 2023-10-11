191163f9264bd5fcf2dec4dc2b9624c48a35cce4
Giovanny Vargas <yovaz05@gmail.com>
2023-10-11 17:53:38 -0500

    Parcial

---
+++ README.md
@@ -0,0 +1,26 @@
+# Examen Parcial
+
+## Desarrollo del proyecto
+
+- Cada componente debe Finalizar con la primera letra del primer nombre y la primera letra del primer apellido (AppGV)
+- Se deberá crear minimo los siguientes componentes
+  - **AppXY**
+  - **QuestionXY**
+  - **ButtonXY (+0.3)**
+- La forma para avanzar entre preguntas
+  - **UseEffect (+0.3)**
+  - **Otra forma (-0,3)**
+- Utlizar estilos al menos en los botones **(+0.3)**
+- Utilizar UseState
+
+Importante saber:
+
+- NO se pueden utilizar otras librerías ni temas que no se han visto, se recomienda proporcionar una solución básica ajustada a lo solicitado, ya que el objetivo principal es evaluar sus habilidades con React y Javascript.
+
+> Happy coding!
+
+![](images/Captura1.PNG)
+
+![](images/Captura2.PNG)
+
+![](images/Captura3.PNG)
---
+++ data.js
@@ -0,0 +1,69 @@
+export const data = [
+  {
+    id: 1,
+    question: "¿Cuándo se inventó la escritura?",
+    answers: [
+      {
+        text: "Hace 20 mil años.",
+        correct: false,
+      },
+      {
+        text: "En el IV milenio a.C.",
+        correct: false,
+      },
+      {
+        text: "En el año 0.",
+        correct: true,
+      },
+      {
+        text: "Siglo XX",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    question: "Según las leyendas de la antiguedad, ¿quiénes fundaron a Roma?",
+    answers: [
+      {
+        text: "Rómulo y Remo.",
+        correct: true,
+      },
+      {
+        text: "Alejandro Magno y Ptolomeo.",
+        correct: false,
+      },
+      {
+        text: "Aquiles y Odiseo.",
+        correct: false,
+      },
+      {
+        text: "Cesar Augustu",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 3,
+    question:
+      "¿Cuál de estos emperadores fue alumno del filósofo griego Aristóteles?",
+    answers: [
+      {
+        text: "Carlomagno",
+        correct: false,
+      },
+      {
+        text: "Trajano",
+        correct: false,
+      },
+      {
+        text: "Cesar",
+        correct: false,
+      },
+      {
+        text: "Alejandro Magno",
+        correct: true,
+      },
+    ],
+  },
+];
